040ad8c20dd9d33d8c87e8aeebbc2590b4c9226e
eyankov98 <122820733+eyankov98@users.noreply.github.com>
2023-01-24 15:33:59 +0200

    test

---
+++ Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (n != 0)
+            {
+                int digit = n % 10;
+                n = n / 10;
+
+                sum = sum + digit;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/Data Types and Variables - Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Data_Types_and_Variables___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((number1 + number2) / number3 * number4);
+
+        }
+    }
+}
