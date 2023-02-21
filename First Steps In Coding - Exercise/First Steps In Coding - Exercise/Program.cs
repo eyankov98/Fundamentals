b86148a3f2440ae4abeecbeddc40fb48d09ad797
eyankov98 <122820733+eyankov98@users.noreply.github.com>
2023-02-21 18:04:46 +0200

    basic

---
+++ First Steps In Coding - Exercise/First Steps In Coding - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Steps_In_Coding___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ First Steps In Coding - Exercise/USD to BGN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
